52271e0046f045649c9cdb7091d6c16b4b07d4da
ibktommy <atomquartz@gmail.com>
2022-05-27 12:16:14 +0100

    Add an Event Listener to the Form Element when the form is submitted

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+// Selecting necessary elements
+const form = document.querySelector("form");
+const input = document.querySelector("input");
 
+// Add Event Listener on Form
+form.addEventListener("submit", (event) => {
+	event.preventDefault();
+});
