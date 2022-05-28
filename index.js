87ece2f26a4ec7f70f0c81b360e990b92a52249f
ibktommy <atomquartz@gmail.com>
2022-05-28 13:00:43 +0100

    Return Encrypted message in the link-input field

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 // Selecting necessary elements
 const form = document.querySelector("form");
-const input = document.querySelector("input");
+const messageInput = document.querySelector("#message-input");
+const linkInput = document.querySelector("#link-input");
 
 // Add Event Listener on Form
 form.addEventListener("submit", (event) => {
 	event.preventDefault();
+
+	// Select Message Input and Encode It
+	const encrypted = btoa(messageInput.value);
+
+	linkInput.value = encrypted;
 });
